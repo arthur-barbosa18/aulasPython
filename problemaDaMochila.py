72683ddff0ba82eed4586d583c9b62a43754abfa
Arthur <arthur.barbosa@loovi.com.br>
2019-02-13 13:32:27 -0200

    create first example

---
+++ problemaDaMochila.py
@@ -0,0 +1,38 @@
+'''
+O problema da mochila consiste em determinar quais dos items de uma lista deverão ser escolhidos para
+serem carregados em uma mochila de capacidade Q (em peso). A cada item da lista é associado um par 
+ordenado nigqii,...,1),,(=, que denotam, respectivamente, o peso e o ganho do i-ésimo item. 
+Aseguir é apresentado um algoritmo heurístico, baseado em uma estratégia gulosa. 
+A complexidade deste algoritmo é O(n log2 n). 
+'''
+
+xablau = True #boolean
+a = 5 #int
+b = 5.66 #float
+c = "string"
+d = [] #lista ou array
+e = () #tupla
+f = {} #dict
+
+adicao = a+b
+subtracao = a-b
+produto = a*b
+divisao = a/b
+resto_da_divisao = a%b
+exponenciação = a**b
+
+#operadores
+
+# and ou &
+# or ou |
+# not ou !
+# comparador de igualdade: ==
+# comparador de diferença: !=
+
+#exemplo
+
+nada =  None
+b = 5
+
+print(a == b)
+print(nada or b)
