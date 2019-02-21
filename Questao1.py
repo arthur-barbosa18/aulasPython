9e26c6cee2d262575c0d36677f42be71cf6237af
KelipN <franafilipe938@gmail.com>
2019-02-21 14:39:02 -0300

    Questao 1 melhor impossivel

--- Questao1.py
+++ Questao1.py
@@ -1,12 +1,27 @@
 nota_invalida = True
-nota = int(input('De uma nota entre 0 e 10\n'))
+while True:
+	try:
+		nota = int(input('De uma nota entre 0 e 10\n'))
+		break
+	except ValueError:
+		print('Um numero carai')
 while(nota_invalida == True):
 	if(nota<0):
 		print('Nota invalida, tente novamente')
-		nota = int(input('De uma nota entre 0 e 10\n'))
+		while True:
+			try:
+				nota = int(input('De uma nota entre 0 e 10\n'))
+				break
+			except ValueError:
+				print('Um numero carai')
 	elif(nota>10):
 		print('Nota invalida, tente novamente')
-		nota = int(input('De uma nota entre 0 e 10\n'))
+		while True:
+			try:
+				nota = int(input('De uma nota entre 0 e 10\n'))
+				break
+			except ValueError:
+				print('Um numero carai')
 	else:
 		nota_invalida = False
 		print('Valor valido')
